dec792064aa1e8a1231223431d4c0a42b478a8b7
Case Wylie <casewylie@Cases-MacBook-Pro.local>
2021-01-05 09:08:09 -0500

    updates for replicating problem

--- server.js
+++ server.js
@@ -73,84 +73,84 @@ app.get("/", (req, res) => {
   res.status(200).send(Tasks);
 });
 
-app.get("/1", (req, res) => {
-  res.status(200).send(1);
+app.get("/test/1", (req, res) => {
+  res.status(200).send("1");
 });
 
-app.get("/2", (req, res) => {
-  res.status(200).send(2);
+app.get("/test/2", (req, res) => {
+  res.status(200).send("2");
 });
 
-app.get("/3", (req, res) => {
-  res.status(200).send(3);
+app.get("/test/3", (req, res) => {
+  res.status(200).send("3");
 });
 
-app.get("/4", (req, res) => {
-  res.status(200).send(4);
+app.get("/test/4", (req, res) => {
+  res.status(200).send("4");
 });
 
-app.get("/5", (req, res) => {
-  res.status(200).send(5);
+app.get("/test/5", (req, res) => {
+  res.status(200).send("5");
 });
 
-app.get("/6", (req, res) => {
-  res.status(200).send(6);
+app.get("/test/6", (req, res) => {
+  res.status(200).send("6");
 });
 
-app.get("/7", (req, res) => {
-  res.status(200).send(7);
+app.get("/test/7", (req, res) => {
+  res.status(200).send("7");
 });
 
-app.get("/8", (req, res) => {
-  res.status(200).send(8);
+app.get("/test/8", (req, res) => {
+  res.status(200).send("8");
 });
 
-app.get("/9", (req, res) => {
-  res.status(200).send(9);
+app.get("/test/9", (req, res) => {
+  res.status(200).send("9");
 });
 
-app.get("/10", (req, res) => {
-  res.status(200).send(10);
+app.get("/test/10", (req, res) => {
+  res.status(200).send("10");
 });
 
-app.get("/11", (req, res) => {
-  res.status(200).send(11);
+app.get("/test/11", (req, res) => {
+  res.status(200).send("11");
 });
 
-app.get("/12", (req, res) => {
-  res.status(200).send(12);
+app.get("/test/12", (req, res) => {
+  res.status(200).send("12");
 });
 
-app.get("/13", (req, res) => {
-  res.status(200).send(13);
+app.get("/test/13", (req, res) => {
+  res.status(200).send("13");
 });
 
-app.get("/14", (req, res) => {
-  res.status(200).send(14);
+app.get("/test/14", (req, res) => {
+  res.status(200).send("14");
 });
 
-app.get("/15", (req, res) => {
-  res.status(200).send(15);
+app.get("/test/15", (req, res) => {
+  res.status(200).send("15");
 });
 
-app.get("/16", (req, res) => {
-  res.status(200).send(16);
+app.get("/test/16", (req, res) => {
+  res.status(200).send("16");
 });
 
-app.get("/17", (req, res) => {
-  res.status(200).send(17);
+app.get("/test/17", (req, res) => {
+  res.status(200).send("17");
 });
 
-app.get("/18", (req, res) => {
-  res.status(200).send(18);
+app.get("/test/18", (req, res) => {
+  res.status(200).send("18");
 });
 
-app.get("/19", (req, res) => {
-  res.status(200).send(19);
+app.get("/test/19", (req, res) => {
+  res.status(200).send("19");
 });
 
-app.get("/20", (req, res) => {
-  res.status(200).send(20);
+app.get("/test/20", (req, res) => {
+  res.status(200).send("20");
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
